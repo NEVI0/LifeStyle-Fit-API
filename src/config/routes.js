40f5994f4bb481b3c590016521525157fc6c1d0a
Nevio <neviocostamagagnin@gmail.com>
2020-04-24 22:07:33 -0300

    New Routes

--- src/config/routes.js
+++ src/config/routes.js
@@ -3,23 +3,28 @@ const express = require('express');
 const AuthCtrl = require('../controllers/AuthCtrl');
 const UserCtrl = require('../controllers/UserCtrl');
 
+const authenticate = require('../config/auth');
+
 module.exports = (server) => {
 
     const authRoute = express.Router();
 	server.use('/auth', authRoute);
 	
 	const userRoute = express.Router();
-    server.use('/user', userRoute);
-
-    // const api = express.Router();
-    // server.use("/api", api);
-    // api.use(auth);
+	server.use('/user', userRoute);
+	// userRoute.use(authenticate);
+	
+	const trackRoute = express.Router();
+	server.use('/track', trackRoute);
+	// trackRoute.use(authenticate);
 
     authRoute.get('/', (req, res) => {
         return res.status(200).json({ message: `API's Running`, status: 200 });
 	});
 	
+	authRoute.post('/signin', AuthCtrl.signIn);
 	authRoute.post('/signup', AuthCtrl.signUp);
+	authRoute.post('/forgotPass', AuthCtrl.forgotPass);
 
 	userRoute.get('/', UserCtrl.users);
 	userRoute.put('/:id', UserCtrl.updateAccount);
